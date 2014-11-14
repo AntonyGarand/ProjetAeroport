a1264bc1093defaf4c16a91a1f2e456737505240
popojargo <alexiscote19@hotmail.com>
2014-11-14 15:56:16 -0500

    Ajoute des entetes de programmation et changement de nom de fichiers
    Rien de particulier you know?

--- ProjetAeroport/ProjetAeroport/FormPrincipal.Designer.cs
+++ ProjetAeroport/ProjetAeroport/FormPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProjetAeroport
 {
-    partial class Form1
+    partial class FormPrincipal
     {
         /// <summary>
         /// Required designer variable.
---
+++ ProjetAeroport/ProjetAeroport/Main.cs
@@ -0,0 +1,34 @@
+﻿// Main.cs 
+// Description du programme : 
+// Programmé par Alexis Coté et Antony Garand ~ Tous drois réservé ~
+// Le : 14 novembre 2014         
+// Historique des modifications
+// Par :
+// Le : 
+// Modif : 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetAeroport
+{
+    /// <summary>
+    /// Gere la creation du form.
+    /// </summary>
+    static class Main
+    {
+        /// <summary>
+        /// Point d'entré du programme
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormPrincipal());
+        }
+    }
+}
