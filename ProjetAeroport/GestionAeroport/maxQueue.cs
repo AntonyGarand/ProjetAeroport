10e06a7cb43f9ebc6d2e1efbc6ca85e7f16ebb98
= <AntonyGarand@gmail.com>
2014-11-28 14:20:22 -0500

    Ajout du hangar et de la Queue avec limite
    Création initialle des classes seulement, pas encore déclarées et
    instanciées dans l'aéroport.

---
+++ ProjetAeroport/GestionAeroport/maxQueue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionAeroport
+{
+    public class MaxQueue<T>:Queue<T>
+    {
+        public MaxQueue(int maximum)
+        {
+            MaxLenght = maximum;
+        }
+
+        new public bool Enqueue(T data)
+        {
+            if (Count > MaxLenght)
+                return false;
+            base.Enqueue(data);
+            return true;
+        }
+
+        public int MaxLenght { get; set; }
+    }
+}
